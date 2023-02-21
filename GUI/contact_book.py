f5405564f8cfff64adede4fa8674757a6e6dc387
Kalinka5 <dania003d@gmail.com>
2023-02-21 17:00:34 +0200

    Update GUI
    Add a lot of classes ImageFrame, ContactFrame, AddContactFrame,
    DepartmentsFrame. Each classes to own frames.

--- GUI/contact_book.py
+++ GUI/contact_book.py
@@ -7,14 +7,15 @@ import re
 import csv
 import os
 import pandas as pd
+from dataclasses import dataclass
 
 
+@dataclass
 class Contact:
-    def __init__(self, first_name, last_name, phone_number, department):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-        self.department = department
+    first_name: str
+    last_name: str
+    phone_number: str
+    department: str
 
     def __lt__(self, other):
         return self.first_name < other.first_name
@@ -32,93 +33,183 @@ class ContactBook(OutputContact):
         self.contacts.append(contact)
 
     def delete_contact(self, contact):
-        self.contacts.pop(contact)
+        self.contacts.remove(contact)
 
     def rename_contact(self, contact, new_name):
         contact.name = new_name
 
 
-class ContactBookGUI(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class ImageFrame(ttk.Frame):
+    def __init__(self, container, tab_control):
+        super().__init__(container)
+        self.tab_control = tab_control
+        self.__create_widgets()
 
-        self.df = None
-        self.i = None
-        self.tree = None
-        self.btn = None
-        self.departments = None
-        self.text3 = None
-        self.t3 = None
-        self.lf = None
-        self.b4 = None
-        self.b3 = None
-        self.b2 = None
-        self.b1 = None
-        self.selected_letter = None
-        self.t1 = None
-        self.t2 = None
-        self.text1 = None
-        self.text2 = None
-        self.txt = None
-        self.dict_departments = {'Work': "0", 'Classmates': "1", 'Friends': "2", 'Relatives': "3", 'Stars': "4"}
+    def __create_widgets(self):
+        self.tab_control.add(self, text='Contact Book')
 
-        self.title('Contact book')
-        self.window_width = 430
-        self.window_height = 320
+        lbl1 = ttk.Label(master=self, text="Your Contact book", foreground='red', font=("Helvetica", 16))
+        lbl1.pack()
 
-        # get the screen dimension
-        self.screen_width = self.winfo_screenwidth()
-        self.screen_height = self.winfo_screenheight()
+        test = tk.PhotoImage(name='telephone', file="images/telephone2.png")
+        label1 = ttk.Label(master=self, image=test)
+        label1.image = test
+        label1.pack()
 
-        # find the center point
-        self.center_x = int(self.screen_width / 2 - self.window_width / 2)
-        self.center_y = int(self.screen_height / 2 - self.window_height / 2)
 
-        # set the position of the window to the center of the screen
-        self.geometry(f'{self.window_width}x{self.window_height}+{self.center_x}+{self.center_y}')
-        self.resizable(True, True)
+class ContactsFrame(ttk.Frame):
+    def __init__(self, container, tab_control, contact_book, tree):
+        super().__init__(container)
+        self.tab_control = tab_control
+        self.__create_widgets()
+        self.contact_book = contact_book
+        self.tree = tree
 
-        self.iconbitmap('images/receiver.ico')
+    def __create_widgets(self):
+        self.tab_control.add(self, text='Contacts')
 
-        # create a folder where templates will be downloaded.
-        if os.path.exists("Contact_book.csv"):
-            self.contact_book_r = open("Contact_book.csv")
-            self.reader = csv.DictReader(self.contact_book_r)
-            print("Open Contact book.")
+        columns = ('first_name', 'last_name', 'number')
+        self.txt = ttk.Treeview(self, columns=columns, show='headings')
+        self.txt.heading('first_name', text='First Name')
+        self.txt.heading('last_name', text='Second Name')
+        self.txt.heading('number', text='Number')
+        self.txt.column('first_name', width=100, anchor=tk.CENTER)
+        self.txt.column('last_name', width=100, anchor=tk.CENTER)
+        self.txt.column('number', width=200, anchor=tk.CENTER)
 
-        self.data = []
-        self.contact_book = ContactBook()
-        self.tab_control = ttk.Notebook(self)
-
-        self.create_tab1()
-        self.create_tab2()
-        self.create_tab3()
-        self.create_tab4()
-
-        self.df = pd.read_csv("Contact_book.csv")
+        df = pd.read_csv("Contact_book.csv")
         # generate sample data
         self.contacts = []
-        if not self.df.empty:
-            for row in self.reader:
-                self.contacts.append((row['first_name'], row['last_name'], row['numbers']))
-                self.contact_book.contacts.append(Contact(row['first_name'],
-                                                          row['last_name'],
-                                                          row['numbers'],
-                                                          row['department']))
-                self.tree.insert('',
-                                 tk.END,
-                                 text=f'{row["first_name"]} {row["last_name"]}',
-                                 iid=str(self.i),
-                                 open=False)
-                self.tree.move(str(self.i), self.dict_departments[row["department"]], 0)
-                self.i += 1
+        if os.path.exists("Contact_book.csv"):
+            contact_book_r = open("Contact_book.csv")
+            reader = csv.DictReader(contact_book_r)
 
-        # add data to the treeview
-        for contact in self.contacts:
-            self.txt.insert('', tk.END, values=contact)
+            if not df.empty:
+                for row in reader:
+                    self.contacts.append((row['first_name'], row['last_name'], row['numbers']))
 
-        self.contact_book_r.close()
-        self.tab_control.pack(expand=1, fill='both')
+            # add data to the treeview
+            for contact in self.contacts:
+                self.txt.insert('', tk.END, values=contact)
+
+            contact_book_r.close()
+
+        self.txt.grid(row=0, column=0, sticky='nsew')
+
+        # add a scrollbar
+        scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.txt.yview)
+        self.txt.configure(yscroll=scrollbar.set)
+        scrollbar.grid(row=0, column=1, sticky='ns')
+
+        # label frame
+        self.lf = ttk.LabelFrame(self, text='Interaction')
+        self.lf.grid(row=1, column=0, sticky='ns')
+
+        self.b2 = ttk.Button(master=self.lf, text='Delete contact', command=self.delete_contact, cursor='hand2')
+        self.b3 = ttk.Button(master=self.lf, text='Rename contact', command=self.rename_contact, cursor='hand2')
+        self.b4 = ttk.Button(master=self.lf, text='Add to favorites', command=self.add_to_favorites, cursor='hand2')
+
+        self.b2.grid(row=1, column=0, sticky='ns')
+        self.b3.grid(row=1, column=1, sticky='ns')
+        self.b4.grid(row=1, column=2, sticky='ns')
+
+    def delete_contact(self):
+        human = self.txt.item(self.txt.focus())['values']
+
+        index = None
+        for n, user in enumerate(self.contact_book.contacts):
+            if human[2] == user.phone_number:
+                index = n
+
+        contact = self.contact_book.contacts[index]
+        self.contact_book.delete_contact(contact)
+
+        selected_item = self.txt.selection()[0]  # get selected item
+        self.txt.delete(selected_item)
+
+        # Search for the row with 'Bob' in the first column
+        for item in self.tree.get_children():
+            print(item)
+            if human[1] in self.tree.item(item, 'values'):
+                self.tree.tag_configure('search', background='yellow')
+                self.tree.tag_add('search', item)
+
+        # Get the ID of the row that contains 'Bob'
+        item_id = self.tree.tag_has('search')
+        print(item_id)
+
+        tk.messagebox.showinfo(title='Update Contact Book',
+                               message=f"\"{human[0]} {human[1]}\" was successfully deleted.")
+
+        print(f"Deleting \"{human[0]} {human[1]}\" from your Contact Book was successfully.\n")
+
+    def rename_contact(self):
+        item = self.txt.item(self.txt.focus())['values']
+        print(item)
+
+        # selection = self.contact_listbox.curselection()
+        # if selection:
+        #     contact_index = selection[0]
+        #     contact = self.contact_book.contacts[contact_index]
+        #     new_name = self.name_entry.get()
+        #     self.contact_book.rename_contact(contact, new_name)
+        #     self.contact_listbox.delete(contact_index)
+        #     self.contact_listbox.insert(contact_index, f"{new_name} - {contact.phone_number}")
+
+    def add_to_favorites(self):
+        item = self.txt.item(self.txt.focus())['values']
+        print(item)
+
+
+class AddContactFrame(ttk.Frame):
+    def __init__(self, container, tab_control, txt, tree, i, dict_departments, contact_book):
+        super().__init__(container)
+        self.tab_control = tab_control
+        self.__create_widgets()
+        self.txt = txt
+        self.tree = tree
+        self.i = i
+        self.dict_departments = dict_departments
+        self.contact_book = contact_book
+
+    def __create_widgets(self):
+        self.tab_control.add(self, text='Add contact')
+
+        lbl3 = ttk.Label(master=self, text='First Name', font=("BOLD", 10))
+        lbl3.grid(row=0, column=0)
+
+        self.text1 = tk.StringVar()
+        self.t1 = ttk.Entry(master=self, textvariable=self.text1)
+        self.t1.focus()
+        self.t1.grid(row=0, column=1)
+
+        lbl4 = ttk.Label(master=self, text='Last Name', font=("BOLD", 10))
+        lbl4.grid(row=1, column=0)
+
+        self.text2 = tk.StringVar()
+        self.t2 = ttk.Entry(master=self, textvariable=self.text2)
+        self.t2.grid(row=1, column=1)
+
+        lbl5 = ttk.Label(master=self, text='Number', font=("BOLD", 10))
+        lbl5.grid(row=2, column=0)
+
+        self.text3 = tk.StringVar()
+        self.t3 = ttk.Entry(master=self, textvariable=self.text3)
+        self.t3.grid(row=2, column=1)
+
+        label = ttk.Label(master=self, text="Departments")
+        label.grid(row=1, column=2)
+        departments_str = tk.StringVar()
+        self.departments = ttk.Combobox(master=self, textvariable=departments_str)
+        headers = ('Work', 'Classmates', 'Friends', 'Relatives', 'Stars')
+        self.departments['values'] = headers
+        self.departments['state'] = 'readonly'
+        self.departments.grid(row=2, column=2)
+        self.departments.bind('<<ComboboxSelected>>', self.get_button_enable)
+
+        self.btn = ttk.Button(master=self, text='Add contact', command=self.add_contact, cursor='hand2')
+        self.btn.grid(row=3, column=1)
+        self.btn.state(['disabled'])
 
     def add_contact(self):
         try:
@@ -161,122 +252,27 @@ class ContactBookGUI(tk.Tk):
             print(nue)
             messagebox.showerror('Number error', 'Number should contain only integers and dashes.')
 
-    def delete_contact(self):
-        item = self.txt.item(self.txt.focus())['values']
-
-        self.contact_book.delete_contact(f"{item[0]} {item[1]}")
-
-        selected_item = self.txt.selection()[0]  # get selected item
-        self.txt.delete(selected_item)
-
-        tk.messagebox.showinfo(title='Update Contact Book',
-                               message=f"\"{item[0]} {item[1]}\" was successfully deleted.")
-
-        print(f"Deleting \"{item[0]} {item[1]}\" from your Contact Book was successfully.\n")
-
-    def rename_contact(self):
-        item = self.txt.item(self.txt.focus())['values']
-        print(item)
-
-    def add_to_favorites(self):
-        item = self.txt.item(self.txt.focus())['values']
-        print(item)
-
     def get_button_enable(self, department):
         # remove the disabled flag
         self.btn.state(['!disabled'])
 
-    def create_tab1(self):
-        tab1 = ttk.Frame(self.tab_control)
-        self.tab_control.add(tab1, text='Contact Book')
 
-        lbl1 = ttk.Label(master=tab1, text="Your Contact book", foreground='red', font=("Helvetica", 16))
-        lbl1.pack()
+class DepartmentsFrame(ttk.Frame):
+    def __init__(self, container, tab_control):
+        super().__init__(container)
+        self.tab_control = tab_control
+        self.dict_departments = {'Work': "0", 'Classmates': "1", 'Friends': "2", 'Relatives': "3", 'Stars': "4"}
+        self.__create_widgets()
 
-        test = tk.PhotoImage(name='telephone', file="images/telephone2.png")
-        label1 = ttk.Label(master=tab1, image=test)
-        label1.image = test
-        label1.pack()
+    def __create_widgets(self):
 
-    def create_tab2(self):
-        tab2 = ttk.Frame(self.tab_control)
-        self.tab_control.add(tab2, text='Contacts')
+        self.tab_control.add(self, text='Departments')
 
-        columns = ('first_name', 'last_name', 'number')
-        self.txt = ttk.Treeview(tab2, columns=columns, show='headings')
-        self.txt.heading('first_name', text='First Name')
-        self.txt.heading('last_name', text='Second Name')
-        self.txt.heading('number', text='Number')
-        self.txt.column('first_name', width=100, anchor=tk.CENTER)
-        self.txt.column('last_name', width=100, anchor=tk.CENTER)
-        self.txt.column('number', width=200, anchor=tk.CENTER)
-
-        self.txt.grid(row=0, column=0, sticky='nsew')
-
-        # add a scrollbar
-        scrollbar = ttk.Scrollbar(tab2, orient=tk.VERTICAL, command=self.txt.yview)
-        self.txt.configure(yscroll=scrollbar.set)
-        scrollbar.grid(row=0, column=1, sticky='ns')
-
-        # label frame
-        self.lf = ttk.LabelFrame(tab2, text='Interaction')
-        self.lf.grid(row=1, column=0, sticky='ns')
-
-        self.b1 = ttk.Button(master=self.lf, text='Add contact', command=self.add_contact)
-        self.b2 = ttk.Button(master=self.lf, text='Delete contact', command=self.delete_contact)
-        self.b3 = ttk.Button(master=self.lf, text='Rename contact', command=self.rename_contact)
-        self.b4 = ttk.Button(master=self.lf, text='Add to favorites', command=self.add_to_favorites)
-
-        self.b2.grid(row=1, column=0, sticky='ns')
-        self.b3.grid(row=1, column=1, sticky='ns')
-        self.b4.grid(row=1, column=2, sticky='ns')
-
-    def create_tab3(self):
-        tab3 = ttk.Frame(self.tab_control)
-        self.tab_control.add(tab3, text='Add contact')
-
-        lbl3 = ttk.Label(master=tab3, text='First Name', font=("BOLD", 10))
-        lbl3.grid(row=0, column=0)
-
-        self.text1 = tk.StringVar()
-        self.t1 = ttk.Entry(master=tab3, textvariable=self.text1)
-        self.t1.focus()
-        self.t1.grid(row=0, column=1)
-
-        lbl4 = ttk.Label(master=tab3, text='Last Name', font=("BOLD", 10))
-        lbl4.grid(row=1, column=0)
-
-        self.text2 = tk.StringVar()
-        self.t2 = ttk.Entry(master=tab3, textvariable=self.text2)
-        self.t2.grid(row=1, column=1)
-
-        lbl5 = ttk.Label(master=tab3, text='Number', font=("BOLD", 10))
-        lbl5.grid(row=2, column=0)
-
-        self.text3 = tk.StringVar()
-        self.t3 = ttk.Entry(master=tab3, textvariable=self.text3)
-        self.t3.grid(row=2, column=1)
-
-        label = ttk.Label(master=tab3, text="Departments")
-        label.grid(row=1, column=2)
-        departments_str = tk.StringVar()
-        self.departments = ttk.Combobox(master=tab3, textvariable=departments_str)
-        headers = ('Work', 'Classmates', 'Friends', 'Relatives', 'Stars')
-        self.departments['values'] = headers
-        self.departments['state'] = 'readonly'
-        self.departments.grid(row=2, column=2)
-        self.departments.bind('<<ComboboxSelected>>', self.get_button_enable)
-
-        self.btn = ttk.Button(master=tab3, text='Add contact', command=self.add_contact)
-        self.btn.grid(row=3, column=1)
-        self.btn.state(['disabled'])
-
-    def create_tab4(self):
-        tab4 = ttk.Frame(self.tab_control)
-        self.tab_control.add(tab4, text='Departments')
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
 
         # create a treeview
-        self.tree = ttk.Treeview(tab4)
+        self.tree = ttk.Treeview(self)
         self.tree.heading('#0', text='Departments', anchor=tk.W)
         headers = ('Work', 'Classmates', 'Friends', 'Relatives', 'Stars')
 
@@ -286,7 +282,91 @@ class ContactBookGUI(tk.Tk):
             self.i += 1
 
         # place the Treeview widget on the root window
-        self.tree.pack()
+        self.tree.grid(row=0, column=0, sticky=tk.NSEW)
+
+        # add a scrollbar
+        scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tree.yview)
+        self.tree.configure(yscroll=scrollbar.set)
+        scrollbar.grid(row=0, column=1, sticky='ns')
+
+        df = pd.read_csv("Contact_book.csv")
+
+        if os.path.exists("Contact_book.csv"):
+            contact_book_r = open("Contact_book.csv")
+            reader = csv.DictReader(contact_book_r)
+
+            if not df.empty:
+                for row in reader:
+                    self.tree.insert('',
+                                     tk.END,
+                                     text=f'{row["first_name"]} {row["last_name"]}',
+                                     iid=str(self.i),
+                                     open=False)
+                    self.tree.move(str(self.i), self.dict_departments[row["department"]], 0)
+                    self.i += 1
+
+            contact_book_r.close()
+
+
+class ContactBookGUI(tk.Tk):
+    def __init__(self):
+        print("Open Contact book.")
+        super().__init__()
+
+        self.title('Contact book')
+        self.window_width = 430
+        self.window_height = 320
+
+        # get the screen dimension
+        self.screen_width = self.winfo_screenwidth()
+        self.screen_height = self.winfo_screenheight()
+
+        # find the center point
+        self.center_x = int(self.screen_width / 2 - self.window_width / 2)
+        self.center_y = int(self.screen_height / 2 - self.window_height / 2)
+
+        # set the position of the window to the center of the screen
+        self.geometry(f'{self.window_width}x{self.window_height}+{self.center_x}+{self.center_y}')
+        self.resizable(True, True)
+
+        self.iconbitmap('images/receiver.ico')
+
+        df = pd.read_csv("Contact_book.csv")
+        self.data = []
+        self.contact_book = ContactBook()
+        self.tab_control = ttk.Notebook(self)
+        self.tab_control.pack(expand=1, fill='both')
+
+        if os.path.exists("Contact_book.csv"):
+            contact_book_r = open("Contact_book.csv")
+            reader = csv.DictReader(contact_book_r)
+
+            if not df.empty:
+                for row in reader:
+                    self.contact_book.contacts.append(Contact(row['first_name'],
+                                                              row['last_name'],
+                                                              row['numbers'],
+                                                              row['department']))
+
+            contact_book_r.close()
+
+        self.__create_widgets()
+
+    def __create_widgets(self):
+        # create the input frame
+        ImageFrame(self, self.tab_control)
+
+        departments_frame = DepartmentsFrame(self, self.tab_control)
+
+        contacts_frame = ContactsFrame(self, self.tab_control, self.contact_book, departments_frame.tree)
+
+        AddContactFrame(self,
+                        self.tab_control,
+                        contacts_frame.txt,
+                        departments_frame.tree,
+                        departments_frame.i,
+                        departments_frame.dict_departments,
+                        self.contact_book)
 
     def save_csv_file(self):
         contact_book_w = open("Contact_book.csv", "w")
