49821fd3a4fdeb31cd2abc724a3826440b8e0048
Kalinka5 <dania003d@gmail.com>
2023-02-22 12:09:23 +0200

    Add new comments in Frames/add_contact.py

--- Frames/add_contact.py
+++ Frames/add_contact.py
@@ -9,41 +9,43 @@ import re
 class AddContactFrame(ttk.Frame):
     def __init__(self, container, tab_control, txt, tree, i, dict_departments, contact_book):
         super().__init__(container)
+        
         self.tab_control = tab_control
-        self.__create_widgets()
         self.txt = txt
         self.tree = tree
         self.i = i
         self.dict_departments = dict_departments
         self.contact_book = contact_book
 
+        self.__create_widgets()
+
     def __create_widgets(self):
         self.tab_control.add(self, text='Add contact')
 
-        lbl3 = ttk.Label(master=self, text='First Name', font=("BOLD", 10))
-        lbl3.grid(row=0, column=0)
+        lbl1 = ttk.Label(master=self, text='First Name', font=("BOLD", 10))
+        lbl1.grid(row=0, column=0)
 
         self.text1 = tk.StringVar()
         self.t1 = ttk.Entry(master=self, textvariable=self.text1)
         self.t1.focus()
         self.t1.grid(row=0, column=1)
 
-        lbl4 = ttk.Label(master=self, text='Last Name', font=("BOLD", 10))
-        lbl4.grid(row=1, column=0)
+        lbl2 = ttk.Label(master=self, text='Last Name', font=("BOLD", 10))
+        lbl2.grid(row=1, column=0)
 
         self.text2 = tk.StringVar()
         self.t2 = ttk.Entry(master=self, textvariable=self.text2)
         self.t2.grid(row=1, column=1)
 
-        lbl5 = ttk.Label(master=self, text='Number', font=("BOLD", 10))
-        lbl5.grid(row=2, column=0)
+        lbl3 = ttk.Label(master=self, text='Number', font=("BOLD", 10))
+        lbl3.grid(row=2, column=0)
 
         self.text3 = tk.StringVar()
         self.t3 = ttk.Entry(master=self, textvariable=self.text3)
         self.t3.grid(row=2, column=1)
 
-        label = ttk.Label(master=self, text="Departments")
-        label.grid(row=1, column=2)
+        lbl4 = ttk.Label(master=self, text="Departments")
+        lbl4.grid(row=1, column=2)
         departments_str = tk.StringVar()
         self.departments = ttk.Combobox(master=self, textvariable=departments_str)
         headers = ('Work', 'Classmates', 'Friends', 'Relatives', 'Stars')
@@ -62,38 +64,59 @@ class AddContactFrame(ttk.Frame):
             last_name = self.text2.get().title()
             number = self.text3.get().replace("-", "")
             pattern = r"(\d{3})(\d{3})([\d.]+)"
+
+            # Get index of contact where he is in contact book by alphabet
+            index = 0
+            while index < len(self.txt.get_children()):
+                if first_name.lower() < self.txt.item(self.txt.get_children()[index])['values'][0].lower():
+                    break
+                index += 1
+
+            # Check name is it has less than 10 letters and more than 0
             if len(first_name) < 1 or len(first_name) > 10:
                 raise NameException(first_name)
+
+            # Check phone number is it has only digits
             elif number.isdigit():
+                # convert phone number to (000)-000-0000
                 result = re.search(pattern, number)
                 number = f"({result[1]})-{result[2]}-{result[3]}"
                 if result:
+
+                    # Add new contact to ContactsFrame
                     self.txt.insert('',
-                                    tk.END,
+                                    index,
                                     values=(first_name, last_name, number))
 
+                    # Add contact to DepartmentsFrame
                     department = self.departments.get()
                     self.tree.insert('', tk.END, text=f'{first_name} {last_name}', iid=str(self.i), open=False)
                     self.tree.move(str(self.i), self.dict_departments[department], 0)
                     self.i += 1
 
+                    # Add contact to class Contacts
                     contact = Contact(first_name, last_name, number, department)
                     self.contact_book.add_contact(contact)
 
+                    # Clear all fields with data
                     self.t1.delete(0, 'end')
                     self.t2.delete(0, 'end')
                     self.t3.delete(0, 'end')
 
+                    # Show message box when add contact
                     tk.messagebox.showinfo(title='Update Contact Book',
                                            message=f"\"{first_name} {last_name}\" was successfully added.")
                     print(f"\"{first_name} {last_name}\" was successfully added to your Contact Book.\n")
             else:
+                # If number contain not only digits, it raises exception
                 raise NumberException(number)
 
         except NameException as ne:
+            # When raise Name error, it shows message box with error text
             print(ne)
             messagebox.showerror('Name error', 'Invalid value of contact name.\nName length should be from 1 to 10.\n')
         except NumberException as nue:
+            # When raise Number error, it shows message box with error text
             print(nue)
             messagebox.showerror('Number error', 'Number should contain only integers and dashes.')
 
