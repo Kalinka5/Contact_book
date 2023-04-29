14f927ff5542bb46290028be5ec431a815c12e75
Kalinka5 <dania003d@gmail.com>
2023-04-29 13:09:29 +0300

    Add new format of numbers different contries.
    Add formating numbers of USA, Canada, Italia, France, Germany, Portugal,
    Poland, UK, Spain.

--- Frames/Contacts/add_frame.py
+++ Frames/Contacts/add_frame.py
@@ -106,29 +106,81 @@ class AddFrame(ttk.Frame):
             last_name = self.text4.get().title()
             number = self.text5.get()
 
-            digits = number.replace("-", "")
+            digits = number.replace("-", "").replace("+", "").replace(" ", "")
 
             if len(digits) < 6 or len(digits) > 12:
                 raise InvalidLengthNumberException(number)
 
             # Check first name is it has less than 10 letters and more than 0
-            if len(first_name) < 1 or len(first_name) > 12:
+            if len(first_name) < 1 or len(first_name) > 15:
                 raise InvalidNameException(first_name)
             # Check last name is it has less than 10 letters and more than 0
             if len(last_name) > 12:
                 raise InvalidNameException(last_name)
 
-            pattern = r"(\d{1})(\d{2})(\d{3})(\d{4})"
-            result = re.search(pattern, digits)
+            result = None
+            normal_number = ""
+            if len(digits) == 10:
+                pattern = r"(\d{1})(\d{2})(\d{3})(\d{4})"
+                result = re.search(pattern, digits)
+                code = f"{result[1]}{result[2]}"
+
+                if digits[:2] == "49":
+                    pattern = r"(\d{2})(\d{2})(\d{6})"
+                    result = re.search(pattern, digits)
+                    # convert phone number to +00-00-000000
+                    normal_number = f"+{result[1]}-{result[2]}-{result[3]}"
+
+                elif code in self.ukrainian_numbers:
+                    # convert phone number to 0-(00)-000-0000
+                    normal_number = f"{result[1]}-({result[2]})-{result[3]}-{result[4]}"
+                else:
+                    raise NotUkrainianCode(code)
+
+            elif len(digits) == 11:
+                pattern = r"(\d{2})(\d{3})(\d{3})(\d{3})"
+                result = re.search(pattern, digits)
+                # convert phone number to +00-000-000-000
+                normal_number = f"+{result[1]}-{result[2]}-{result[3]}-{result[4]}"
+                if digits[:1] == "1":
+                    pattern = r"(\d{1})(\d{3})(\d{3})(\d{4})"
+                    result = re.search(pattern, digits)
+                    # convert phone number to +0-000-000-0000
+                    normal_number = f"+{result[1]}-{result[2]}-{result[3]}-{result[4]}"
+
+            elif len(digits) == 12:
+                pattern = r"(\d{3})(\d{2})(\d{3})(\d{4})"
+                result = re.search(pattern, digits)
+
+                code = f"{result[1][-1]}{result[2]}"
+                if result[1] == "380":
+                    if code in self.ukrainian_numbers:
+                        # convert phone number to 0-(00)-000-0000
+                        normal_number = f"{result[1][-1]}-({result[2]})-{result[3]}-{result[4]}"
+                elif result[1] == "351":
+                    pattern = r"(\d{3})(\d{3})(\d{3})(\d{3})"
+                    result = re.search(pattern, digits)
+                    # convert phone number to +000-000-000-000
+                    normal_number = f"+{result[1]}-{result[2]}-{result[3]}-{result[4]}"
+                elif result[1] == "393":
+                    pattern = r"(\d{2})(\d{3})(\d{7})"
+                    result = re.search(pattern, digits)
+                    # convert phone number to +00-000-0000000
+                    normal_number = f"+{result[1]}-{result[2]}-{result[3]}"
+                elif digits[:2] == "44":
+                    pattern = r"(\d{2})(\d{4})(\d{6})"
+                    result = re.search(pattern, digits)
+                    # convert phone number to +00-0000-000000
+                    normal_number = f"+{result[1]}-{result[2]}-{result[3]}"
+                else:
+                    pattern = r"(\d{2})(\d{3})(\d{7})"
+                    result = re.search(pattern, digits)
+                    # convert phone number to +00-0000-000000
+                    normal_number = f"+{result[1]}-{result[2]}-{result[3]}"
+
             # If number contain not only digits, it raises exception
             if not result:
                 raise InvalidNumberException(number)
-
-            if f"{result[1]}{result[2]}" in self.ukrainian_numbers:
-                # convert phone number to 0-(00)-000-0000
-                normal_number = f"{result[1]}-({result[2]})-{result[3]}-{result[4]}"
-            else:
-                raise NotUkrainianCode(result[2])
 
             # check is number exist in the Contact Book
             all_numbers = self.contact_book.get_all_numbers
